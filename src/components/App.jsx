34ce7ca669fcbcdbf2cac782b76f3daba5c6126f
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2024-05-07 22:16:34 -0230

    contacts adding and render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,70 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+    name: '',
+    number: '',
+  };
+
+  onInputChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+    const { name, number } = this.state;
+    const contact = {
+      name,
+      number,
+      id: nanoid(),
+    };
+    this.setState(prev => {
+      return { contacts: [contact, ...prev.contacts], name: '', number: '' };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onFormSubmit}>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.onInputChange}
+            value={this.state.name}
+          />
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            onChange={this.onInputChange}
+            value={this.state.number}
+          />
+          <button type="submit">Add contact</button>
+        </form>
+        <ul>
+          {this.state.contacts.map(el => {
+            return (
+              <li key={el.id}>
+                <p>{el.name}</p>
+                <p>{el.number}</p>
+                <button>Remove</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export { App };
