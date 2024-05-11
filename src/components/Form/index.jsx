4ec3ead20b0e1c1f0dcfd5483dbf3eedd198ab55
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2024-05-11 17:53:16 -0230

    validation updated

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -47,7 +47,7 @@ class Form extends Component {
             className="input"
             type="tel"
             name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
             onChange={this.onInputChange}
